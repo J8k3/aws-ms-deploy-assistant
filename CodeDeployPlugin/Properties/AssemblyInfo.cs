eaa395509f56c19ddfb98909c3272375a7764a26
J8k3 <jacob.marks@gmail.com>
2026-02-25 17:47:50 -0500

    Replaced old AssemblyInfo Files

--- CodeDeployPlugin/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("AWS Deployment Assistant CodeDeployPlugin")]
-[assembly: AssemblyDescription("This plugin prepares build output for deployment with CodeDeploy.")]
-[assembly: Guid("1f737f79-b171-4d30-8ea1-daa04fca0903")]
